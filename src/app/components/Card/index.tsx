3ee2ed6402a0b502aefc1cffb66c82543745ef0d
Valfran <valfran-goncalves@hotmail.com>
2025-07-29 17:34:52 -0300

    chore: ajustando card e card module css

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -8,22 +8,23 @@ type Props = {
 };
 
 const Card = ({ artigos }: Props) => {
-  const { source, author, title, description, url, urlToImage } = artigos;
+  const { author, title, description } = artigos;
 
   return (
     <div className={styles.card}>
       <Link href={`/artigos/${gerarSlug(title)}`}>
-          <img
-            className={styles.card__image}
-            src={artigos.urlToImage}
-            alt={artigos.title}
-          />
-          <div className={styles.content}>
-            <h3 className={styles.title}>{title}</h3>
-            <p>{source.name}</p>
-            <p className={styles.description}>{description}</p>
-            <p>{author}</p>
-            <button className={styles.card_button}> Ler mais</button>
+        <img
+          className={styles.card__image}
+          src={artigos.urlToImage}
+          alt={artigos.title}
+        />
+        <div className={styles.card__content}>
+          <h3 className={styles.card__title}>{title}</h3>
+          <p className={styles.card__description}>{description}</p>
+          <div className={styles.card__footer}>
+            <p className={styles.card__author}>{author}</p>
+            <button className={styles.card__button}> Ler mais<img src="/Vector.png" alt="arrow right"/></button>
+          </div>
         </div>
       </Link>
     </div>
