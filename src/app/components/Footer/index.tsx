6bb788755191512481aa4767a3a8c304dd312ff4
Valfran <valfran-goncalves@hotmail.com>
2025-07-29 07:45:18 -0300

    feat: adicionando componente Footer e css module

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <p>&copy; 2025 Valfran Jr. Todos os direitos reservados.</p>
+
+    </footer>
+  );
+};
+
+export default Footer;
